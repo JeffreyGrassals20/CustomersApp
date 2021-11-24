aff9381a842081fd40e580ff9e04a0640cba4cf3
Jeffrey <slassarg20@gmail.com>
2021-11-24 00:22:19 -0400

    Create user view created with all styles

--- CustomersApp/Controllers/CustomersController.cs
+++ CustomersApp/Controllers/CustomersController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CustomersApp.Models;
+
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,6 +14,11 @@ namespace CustomersApp.Controllers
     {
         // GET: /<controller>/
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
