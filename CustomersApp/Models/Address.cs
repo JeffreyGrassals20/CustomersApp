794b3bbb688e70d9db522a9c8b82868bd450b5a2
Jeffrey <slassarg20@gmail.com>
2021-11-23 23:39:46 -0400

    Customers and Address models files created with controllers files

---
+++ CustomersApp/Models/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomersApp.Models
+{
+    public class Address
+    {
+        public Address()
+        {
+        }
+
+        [Required (ErrorMessage ="Customer ID Is Required")]
+        public int CustomerID { get; set; }
+
+        [Required(ErrorMessage = "Address Is Required")]
+        public string Addresses { get; set; }
+    }
+}
---
+++ CustomersApp/Models/Customers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomersApp.Models
+{
+    public class Customers
+    {
+        public Customers()
+        {
+        }
+
+        [Required (ErrorMessage ="Customer Name Is Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Customer Last Name Is Required")]
+        public string LastName { get; set; }
+
+        [Required (ErrorMessage="Customer ID number is Required")]
+        [StringLength(11)]
+        public string DocumentID { get; set; }
+
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
